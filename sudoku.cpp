2688915db5ae90a4ab9a04d98cf8787e027c7070
rain0r <rainer@hihn.org>
2014-04-16 18:21:38 +0200

    beta version

--- sudoku.cpp
+++ sudoku.cpp
@@ -10,19 +10,18 @@ using namespace std;
 typedef boost::multi_array<int,2> barray;
 barray start_sudoku_field(boost::extents[9][9]);
 list<barray> field_stack;
-bool end = false;
 
 void init_multi_dim_array() {
     int init_val[9][9] = {
-        { 1, 0, 0, 4, 0, 0, 5, 0, 0 },
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        { 4, 0, 0, 8, 0, 0, 9, 0, 0 },
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+        { 2, 3, 4, 5, 6, 7, 8, 9, 1 },
+        { 3, 4, 5, 6, 7, 8, 9, 1, 2 },
+        { 4, 5, 6, 7, 8, 9, 1, 2, 3 },
+        { 5, 6, 7, 8, 9, 1, 2, 3, 4 },
+        { 6, 7, 8, 9, 1, 2, 3, 4, 5 },
         { 7, 0, 0, 5, 0, 0, 4, 0, 0 },
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+        { 8, 9, 1, 2, 3, 4, 5, 6, 7 },
+        { 9, 1, 2, 3, 4, 5, 6, 7, 0 }
     };
 
     for (int i = 0; i < 9; i++) {
@@ -33,7 +32,66 @@ void init_multi_dim_array() {
 
 }
 
-bool solved(barray field) {
+// Count the appearance of no in row
+int get_row_elem_count(barray field, int row, int no) {
+    int count = 0;
+
+    for (int i = 0; i < 9; i++) {
+        if (field[row][i] == no) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+// Count the appearance of no in col
+int get_col_elem_count(barray field, int col, int no) {
+    int count = 0;
+
+    for (int i = 0; i < 9; i++) {
+        if (field[i][col] == no) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+// checks for duplicates in each quadrat
+// returns true if there arent any
+bool check_all_quadrat(barray field) {
+    int x_y_quad[3] = { 0, 3, 6 };
+    int count = 0;
+
+    for (int k = 1; k <= 9; k++) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                count = 0;
+                for (int y = x_y_quad[i]; y <= (x_y_quad[i]+2); y++) {
+                    for (int x = x_y_quad[j]; x <= (x_y_quad[j]+2); x++) {
+                        if (k == field[x][y]) {
+                            if (count > 0) {
+                                cout << "Duplicates in a quadrat: ";
+                                cout << k << " at (";
+                                cout << x_y_quad[i]+1 << "|";
+                                cout << x_y_quad[j]+1 << ")" << endl;
+                                return false;
+                            }
+                            count++;
+                        }
+                    }
+                }
+
+
+            }
+        }
+    }
+}
+
+bool is_solved(barray field) {
+    // checks for zeroed fields
+    // any field[x][y] == 0 means the game is not yet finished
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
             if (field[i][j] == 0) {
@@ -42,7 +100,28 @@ bool solved(barray field) {
         }
     }
 
+    // check all cols and rows
+    for (int i = 1; i <= 9; i++) {
 
+        for (int j = 0; j < 9; j++) {
+            if (get_row_elem_count(field, j, i) > 1) {
+                cout << "Duplicate Number in row " << j+1 << ": " << i << endl;
+                return false;
+            }
+        }
+
+        for (int k = 0; k < 9; k++) {
+            if (get_col_elem_count(field, k, i) > 1) {
+                cout << "Duplicate Number in column " << k+1 << ": " << i << endl;
+                return false;
+            }
+        }
+    }
+
+    // Check all quadrat
+    if (!check_all_quadrat(field)) {
+        return false;
+    }
 }
 
 void print_matrix(barray field) {
@@ -64,15 +143,24 @@ void print_matrix(barray field) {
 }
 
 void algo(barray field) {
+    int count = 0;
+    field_stack.push_back(field);
 
+    list<barray>::iterator iter;
+    for (iter = field_stack.begin(); iter != field_stack.end(); iter++) {
+        if (is_solved(*iter)) {
+            cout << "Solved" << endl;
+            return;
+        }
 
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            if (field[i][j] == 0) {
-                for (int k = 1; k <= 9; k++) {
-                    barray copy = field;
-                    copy[i][j] = k;
-                    field_stack.push_back(copy);
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if ((*iter)[i][j] == 0) {
+                    for (int k = 1; k <= 9; k++) {
+                        barray copy = *iter;
+                        copy[i][j] = k;
+                        field_stack.push_back(copy);
+                    }
                 }
             }
         }
@@ -82,12 +170,15 @@ void algo(barray field) {
 int main(int argc, char* argv[]) {
     cout << "Blatt01/Aufgabe 2" << endl;
 
-    init_multi_dim_array();
-    field_stack.push_back(start_sudoku_field);
+    boost::lockfree::queue<int> queue(128);
+    int thread_count = 1;
+    boost::thread_group sudoku_threads;
 
-    while (!end) {
-        algo(start_sudoku_field);
-        print_matrix( field_stack.back() );
+    init_multi_dim_array();
+    algo(start_sudoku_field);
+
+    for (int i = 0; i != thread_count; i++) {
+        sudoku_threads.add_thread(new boost::thread(algo, start_sudoku_field));
     }
 
     return 0;
